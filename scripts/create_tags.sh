805573fb1bb2c397c458ac0d6089bdd45a49bedc
Ivan Kotenkov <kotenkov@yandex-team.ru>
2014-02-25 17:59:06 +0400

    Add scripts.

---
+++ scripts/create_tags.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+ctags_cmd='ctags -R --c++-kinds=+p --fields=+iaS --extra=+qf --languages=C,C++'
+$ctags_cmd --exclude='out' --exclude='out' --exclude='.git' --exclude='third_party'
+$ctags_cmd -a third_party/WebKit
+
+# Dumb fix for tags that are too long.
+cat tags | awk "length < 512" > .tags
+mv .tags tags
+
