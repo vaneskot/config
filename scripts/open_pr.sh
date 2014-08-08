2d1d1e3fa77c44d55aeada45bfa30f3f26e195ef
Ivan Kotenkov <kotenkov@yandex-team.ru>
2014-08-08 16:24:52 +0400

    Add script for pull-request opening.

---
+++ scripts/open_pr.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+merge_commit=`git rev-list --ancestry-path --merges "$1..origin/master" | tail -1`
+commit_header=`git log -1 --pretty=format:%s $merge_commit`
+echo $commit_header
+platform=`uname`
+if [[ $platform == 'Darwin' ]]; then
+  open "https://stash.desktop.dev.yandex.net/projects/STARDUST/repos/browser/pull-requests/`echo $commit_header | awk '{print substr($4, 2, length($4) - 1)}'`"
+fi
