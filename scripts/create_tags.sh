cf1f6d13da4c9217ff3a59b65fa975bfca783fc2
Ivan Kotenkov <kotenkov@yandex-team.ru>
2015-12-16 07:43:08 +0100

    Scripts: add skia to tags.

--- scripts/create_tags.sh
+++ scripts/create_tags.sh
@@ -2,6 +2,7 @@
 ctags_cmd='ctags -R --c++-kinds=+p --fields=+iaS --extra=+qf --languages=C,C++'
 $ctags_cmd --exclude='out' --exclude='out' --exclude='.git' --exclude='third_party'
 $ctags_cmd -a third_party/WebKit
+$ctags_cmd -a third_party/skia
 
 # Dumb fix for tags that are too long.
 cat tags | awk "length < 512" > .tags
