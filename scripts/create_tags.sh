33a4af3d8da273ed5822e12d41c3700836693fd1
Ivan Kotenkov <kotenkov@yandex-team.ru>
2016-11-09 16:47:59 +0100

    Scripts: add dictless_lemmer to tags.

--- scripts/create_tags.sh
+++ scripts/create_tags.sh
@@ -3,6 +3,7 @@ ctags_cmd='ctags -R --c++-kinds=+p --fields=+iaS --extra=+qf --languages=C,C++'
 $ctags_cmd --exclude='out' --exclude='out' --exclude='.git' --exclude='third_party'
 $ctags_cmd -a third_party/WebKit
 $ctags_cmd -a third_party/skia
+$ctags_cmd -a third_party/dictless_lemmer
 
 # Dumb fix for tags that are too long.
 cat tags | awk "length < 512" > .tags
