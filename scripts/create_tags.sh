4262355d106d831b2d78670057074d622f8f34d2
Ivan Kotenkov <kotenkov@yandex-team.ru>
2018-07-12 12:14:29 +0300

    tags: add java and dont traverse unnecessary dirs.

--- scripts/create_tags.sh
+++ scripts/create_tags.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-ctags_cmd='ctags -R --c++-kinds=+p --fields=+iaS --extra=+qf --languages=C,C++'
+ctags_cmd='ctags -R --c++-kinds=+p --fields=+iaS --extra=+qf --languages=C,C++,Java'
 $ctags_cmd --exclude='out' --exclude='.git' --exclude='third_party' --exclude='browsec'
-$ctags_cmd -a third_party/WebKit
-$ctags_cmd -a third_party/skia
-$ctags_cmd -a third_party/dictless_lemmer
+$ctags_cmd -a third_party/blink
 
 # Dumb fix for tags that are too long.
 cat tags | awk "length < 512" > .tags
 mv .tags tags
-
